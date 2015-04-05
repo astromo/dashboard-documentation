192987f1442d650e496be2a000c2646d1fa6f3a1
Gilles De Mey <gilles.de.mey@gmail.com>
2015-04-05 02:05:28 +0200

    Updated docs

---
+++ controllers/blueprint.js
@@ -0,0 +1,45 @@
+'use strict';
+
+angular.module('astromo.docs')
+  .controller('docs.blueprintController', function($scope, $stateParams, Restangular) {
+    console.log('and blueprint');
+
+    var slug = $stateParams.blueprint;
+
+    function mapBlueprint(blueprint) {
+
+      $scope.current.name = blueprint.name;
+
+      var api    = blueprint.ast;
+
+      $scope.api = {
+        name        : api.name,
+        description : api.description
+      };
+
+      $scope.api.categories = _.where(api.content, { 'element': 'category' });
+
+      // map categories
+      _.map($scope.api.categories, function(category) {
+        category.description = _.chain(category.content)
+          .where({ 'element': 'copy' })
+          .map(function(elem) { return elem.content; })
+          .value();
+      });
+
+      // map resources
+      _.map($scope.api.categories, function(category) {
+        category.resources = _.chain(category.content)
+          .where({ 'element': 'resource' })
+          .value();
+      });
+
+      // map actions
+      _.map($scope.api.categories, function(category) {
+        category.resources.actions = category.content.actions;
+      });
+
+    }
+
+    Restangular.one('blueprints', slug).get().then(mapBlueprint);
+  });
--- controllers/docs.js
+++ controllers/docs.js
@@ -1,7 +1,21 @@
 'use strict';
 
 angular.module('astromo.docs')
-  .controller('docs.mainController', function($scope) {
+  .controller('docs.mainController', function($scope, blueprints, $state) {
     console.log('and documentation');
+
+    // TODO: figure out last active blueprint
+    $scope.lastActive    = null;
+
+    $scope.blueprints    = blueprints || [];
+    $scope.blueprint     = $scope.lastActive || blueprints[0];
+
+    $scope.hasBlueprints = blueprints.length > 0;
+
+    $scope.current = {
+      name: $scope.blueprint.name
+    };
+
+    $(document).foundation('dropdown', 'reflow'); // reflow dropdown
   });
 
